368d269d38eef8f2ef940cd7c574d91d353a80d3
webhero-nur <nurealom522@gmail..com>
2022-09-13 18:08:33 +0600

    array element access using loop understand

---
+++ array-loop.js
@@ -0,0 +1,18 @@
+var numbers = [54, 69, 87, 46, 51, 68, 44];
+for (var i = 0; i < 5; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
+
+for (var j = 0; j < numbers.length; j++) {
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['laptop', 'monitor', 'mouse', 'keyboard', 'earphone', 'headphone', 'mouse pad', 'watch', 'laptop stand', 'hdmi cable', 'phone charger'];
+var m = 0;
+while (items[m] != null) {
+    var item = items[m];
+    console.log((m + 1) + '. ' + item);
+    m++;
+}
