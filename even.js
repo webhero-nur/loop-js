3f86481272682e4712bc3485b9931c4edf793d68
webhero-nur <nurealom522@gmail..com>
2022-09-13 13:29:19 +0600

    even-odd using loop understood

---
+++ even.js
@@ -0,0 +1,8 @@
+// even number from 0 to 20
+var number = 0;
+while (number <= 20) {
+    if (number % 2 == 0) {
+        console.log(number);
+    }
+    number++;
+}
---
+++ numbers.js
@@ -0,0 +1,5 @@
+var number = 1;
+while (number <= 20) {
+    console.log(number);
+    number++;
+}
