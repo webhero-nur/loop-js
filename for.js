d25e8f5b5f5f2d5818a7a1b81d38a965590c2873
webhero-nur <nurealom522@gmail..com>
2022-09-13 13:37:20 +0600

    for loop understood

---
+++ for.js
@@ -0,0 +1,4 @@
+for (var birianiGiven = 0; birianiGiven < 50; birianiGiven++) {
+    console.log("Roast Den, Please !!");
+    console.log(birianiGiven);
+}
---
+++ time-table.js
@@ -0,0 +1,5 @@
+var table = 15;
+console.log("Time Table of " + table + ":");
+for (var i = 1; i <= 10; i++) {
+    console.log(table + " X " + i + " = " + table * i);
+}
